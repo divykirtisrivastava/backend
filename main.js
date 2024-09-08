b052571717068a6a564f6916cabdb64b0ac3cd54
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-09-08 23:35:41 +0530

    aded

--- main.js
+++ main.js
@@ -177,11 +177,6 @@ app.delete('/deleteusers/:id', (req, res) => {
   });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 const createDummyTable = `
   CREATE TABLE if not exists usersdata (
